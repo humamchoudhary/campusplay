d93e12b759592192685f30b1ffd7b1da8caf603c
Muhammad Humam <humamchoudhary@gmail.com>
2025-01-17 00:31:19 +0500

    Update

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,6 +5,7 @@ import {
   ScrollView,
   TouchableOpacity,
   TextInput,
+  Dimensions,
 } from "react-native";
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
@@ -19,6 +20,8 @@ import {
   Send,
   ThumbsUp,
 } from "lucide-react-native";
+
+import { Menu as SMenu, Provider, Divider } from "react-native-paper";
 import { router } from "expo-router";
 import { useState } from "react";
 import { useSession } from "@/context/SessionContext";
@@ -44,106 +47,245 @@ function Post({
 }: Post) {
   const { user } = useSession();
   const colorScheme = useColorScheme();
+  const [visible, setVisible] = useState(false);
+  const openMenu = () => setVisible(true);
+  const closeMenu = () => setVisible(false);
+  const { height, width } = Dimensions.get("window");
+
+  const [editModal, setEditModal] = useState(false);
+  const handleEdit = () => {
+    closeMenu();
+    // console.log("Edit clicked");
+    setEditModal(true);
+    // Add your edit logic here
+  };
+  const edit_form = useForm({
+    defaultValues: {
+      caption: caption,
+      image: image,
+    },
+  });
+
+  const handleDelete = () => {
+    closeMenu();
+    console.log("Delete clicked");
+    // Add your delete logic here
+  };
   return (
-    <ThemedView className="flex flex-col gap-4 w-full h-fit my-2">
-      <ThemedView className="flex flex-row px-4 justify-between w-full">
-        <ThemedView className="flex flex-row gap-4 items-center w-full">
-          <ThemedView
-            style={{ backgroundColor: Colors["light"].tint }}
-            className="w-14 h-14 rounded-full"
+    <Provider>
+      <ThemedView className="flex flex-col gap-4 w-full h-fit my-2">
+        <ThemedView className="flex flex-row px-4 justify-between w-full">
+          <ThemedView className="flex flex-row gap-4 items-center w-full">
+            <ThemedView
+              style={{ backgroundColor: Colors["light"].tint }}
+              className="w-14 h-14 rounded-full"
+            />
+            <ThemedView
+              className="flex flex-row justify-between items-center w-min"
+              style={{ flex: 1 }}
+            >
+              <ThemedView className="flex flex-col justify-center">
+                <ThemedText className="font-bold w-min">{username}</ThemedText>
+                <ThemedText
+                  className="font-extralight w-min"
+                  style={{
+                    color: Colors[colorScheme ?? "light"].text + "80",
+                    fontSize: 12,
+                  }}
+                >
+                  {time}
+                </ThemedText>
+              </ThemedView>
+              {user && user.loggedin === "admin" ? (
+                <SMenu
+                  visible={visible}
+                  onDismiss={closeMenu}
+                  anchor={{
+                    x: width,
+                    y: 60,
+                  }}
+                  anchorPosition="top"
+                  contentStyle={{
+                    backgroundColor: Colors[colorScheme ?? "light"].background,
+                    borderColor: Colors[colorScheme ?? "light"].background_sec,
+                    borderWidth: 1,
+                  }}
+                >
+                  <SMenu.Item onPress={handleEdit} title="Edit" />
+                  <Divider />
+                  <SMenu.Item onPress={handleDelete} title="Delete" />
+                </SMenu>
+              ) : null}
+
+              <TouchableOpacity
+                activeOpacity={0.8}
+                onPress={openMenu}
+                style={{
+                  padding: 3,
+                }}
+              >
+                <Ellipsis color={Colors[colorScheme ?? "light"].text} />
+              </TouchableOpacity>
+            </ThemedView>
+          </ThemedView>
+        </ThemedView>
+        <ThemedText className="px-4" type="defaultSemiBold">
+          {caption}
+        </ThemedText>
+        <ThemedView className="relative items-center justify-center">
+          <Image
+            src={image}
+            resizeMode="contain"
+            style={{
+              width: "100%", // Full screen width
+              height: undefined, // Automatically adjust height to maintain aspect ratio
+              maxHeight: 360, // Limit the height to 360px
+              aspectRatio: 16 / 9, // Optional: Maintain a specific aspect ratio
+            }}
           />
           <ThemedView
-            className="flex flex-row justify-between items-center w-min"
-            style={{ flex: 1 }}
+            className="border rounded-lg absolute -bottom-5 shadow-md px-4 flex w-5/6 flex-row gap-2 justify-evenly items-center"
+            style={{
+              borderColor: Colors[colorScheme ?? "light"].tabIconDefault + "1A",
+            }}
           >
-            <ThemedView className="flex flex-col justify-center">
-              <ThemedText className="font-bold w-min">{username}</ThemedText>
+            <ThemedView className="flex flex-row gap-3 items-center justify-center px-5 py-2">
+              <ThumbsUp
+                color={Colors[colorScheme ?? "light"].tabIconDefault}
+                size={16}
+              />
               <ThemedText
-                className="font-extralight w-min"
                 style={{
-                  color: Colors[colorScheme ?? "light"].text + "80",
+                  color: Colors[colorScheme ?? "light"].tabIconDefault,
                   fontSize: 12,
                 }}
               >
-                {time}
+                {likes}
               </ThemedText>
             </ThemedView>
-            <TouchableOpacity activeOpacity={0.8} className="p-3">
-              <Ellipsis color={Colors[colorScheme ?? "light"].text} />
-            </TouchableOpacity>
+
+            <ThemedView className="flex flex-row gap-3 items-center justify-center px-5 py-2">
+              <MessageSquare
+                color={Colors[colorScheme ?? "light"].tabIconDefault}
+                size={16}
+              />
+              <ThemedText
+                style={{
+                  color: Colors[colorScheme ?? "light"].tabIconDefault,
+                  fontSize: 12,
+                }}
+              >
+                {comments}
+              </ThemedText>
+            </ThemedView>
+
+            <ThemedView className="flex flex-row gap-3 items-center justify-center px-5 py-2">
+              <Forward
+                color={Colors[colorScheme ?? "light"].tabIconDefault}
+                size={16}
+              />
+              <ThemedText
+                style={{
+                  color: Colors[colorScheme ?? "light"].tabIconDefault,
+                  fontSize: 12,
+                }}
+              >
+                {shares}
+              </ThemedText>
+            </ThemedView>
           </ThemedView>
         </ThemedView>
       </ThemedView>
-      <ThemedText className="px-4" type="defaultSemiBold">
-        {caption}
-      </ThemedText>
-      <ThemedView className="relative items-center justify-center">
-        <Image
-          src={image}
-          resizeMode="contain"
-          style={{
-            width: "100%", // Full screen width
-            height: undefined, // Automatically adjust height to maintain aspect ratio
-            maxHeight: 360, // Limit the height to 360px
-            aspectRatio: 16 / 9, // Optional: Maintain a specific aspect ratio
-          }}
-        />
+
+      <Modal isVisible={editModal}>
         <ThemedView
-          className="border rounded-lg absolute -bottom-5 shadow-md px-4 flex w-5/6 flex-row gap-2 justify-evenly items-center"
           style={{
-            borderColor: Colors[colorScheme ?? "light"].tabIconDefault + "1A",
+            backgroundColor: Colors[colorScheme ?? "light"].background,
+            borderColor: Colors[colorScheme ?? "light"].tint,
+            borderWidth: 1,
+            width: "100%",
           }}
+          className="flex flex-col rounded-md px-4 py-8 items-center gap-4"
         >
-          <ThemedView className="flex flex-row gap-3 items-center justify-center px-5 py-2">
-            <ThumbsUp
-              color={Colors[colorScheme ?? "light"].tabIconDefault}
-              size={16}
-            />
-            <ThemedText
-              style={{
-                color: Colors[colorScheme ?? "light"].tabIconDefault,
-                fontSize: 12,
-              }}
-            >
-              {likes}
-            </ThemedText>
-          </ThemedView>
+          <ThemedText style={{ fontWeight: 700, fontSize: 24 }}>
+            Edit Post
+          </ThemedText>
+          <edit_form.Field
+            name="caption"
+            validators={{
+              onSubmitAsync: ({ value }) => {
+                return !value ? "Caption is required" : undefined;
+              },
+            }}
+          >
+            {(field) => (
+              <TextInput
+                className="border rounded-md w-full "
+                placeholder="Coach Name"
+                placeholderTextColor={
+                  Colors[colorScheme ?? "light"].tabIconDefault
+                }
+                value={field.state.value}
+                onChangeText={field.handleChange}
+                style={{
+                  borderColor:
+                    field.state.meta.errors && field.state.meta.errors.length
+                      ? "#F33"
+                      : Colors[colorScheme ?? "light"].tabIconDefault + "80",
+                  color:
+                    field.state.meta.errors && field.state.meta.errors.length
+                      ? "#F33"
+                      : Colors[colorScheme ?? "light"].text,
+                  paddingHorizontal: 10,
+                  // paddingHorizontal: 10,
+                  paddingVertical: 15,
+                }}
+              />
+            )}
+          </edit_form.Field>
 
-          <ThemedView className="flex flex-row gap-3 items-center justify-center px-5 py-2">
-            <MessageSquare
-              color={Colors[colorScheme ?? "light"].tabIconDefault}
-              size={16}
-            />
-            <ThemedText
-              style={{
-                color: Colors[colorScheme ?? "light"].tabIconDefault,
-                fontSize: 12,
-              }}
-            >
-              {comments}
-            </ThemedText>
-          </ThemedView>
+          <edit_form.Field
+            name="image"
+            validators={{
+              onSubmitAsync: ({ value }) => {
+                return !value ? "Image is required" : undefined;
+              },
+            }}
+          >
+            {(field) => (
+              <TouchableOpacity activeOpacity={0.9}>
+                <Image
+                  src={image}
+                  resizeMode="contain"
+                  style={{
+                    width: "100%", // Full screen width
+                    height: undefined, // Automatically adjust height to maintain aspect ratio
+                    maxHeight: 360, // Limit the height to 360px
+                    aspectRatio: 16 / 9, // Optional: Maintain a specific aspect ratio
+                  }}
+                />
+              </TouchableOpacity>
+            )}
+          </edit_form.Field>
 
-          <ThemedView className="flex flex-row gap-3 items-center justify-center px-5 py-2">
-            <Forward
-              color={Colors[colorScheme ?? "light"].tabIconDefault}
-              size={16}
-            />
-            <ThemedText
-              style={{
-                color: Colors[colorScheme ?? "light"].tabIconDefault,
-                fontSize: 12,
-              }}
-            >
-              {shares}
-            </ThemedText>
-          </ThemedView>
+          <TouchableOpacity
+            onPress={() => {
+              setEditModal(false);
+            }}
+            activeOpacity={0.8}
+            className="rounded-md w-full py-4 items-center"
+            style={{ backgroundColor: "#d20f39" }}
+          >
+            <ThemedText>Cancel</ThemedText>
+          </TouchableOpacity>
         </ThemedView>
-      </ThemedView>
-    </ThemedView>
+      </Modal>
+    </Provider>
   );
 }
 
+import Modal from "react-native-modal";
+import { useForm } from "@tanstack/react-form";
 export default function HomeScreen() {
   // INITIALIZATIONS
   const [newPostCaption, setNewPostCaption] = useState<string>();
